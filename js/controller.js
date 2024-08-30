b95b1d9a260f8c117efb608f0b0a7ebd626a9593
Gustavo Kophal <17kophal@gmail.com>
2024-08-30 19:12:42 -0300

    mudanças no tutorial e nas teclas WASD

--- js/controller.js
+++ js/controller.js
@@ -4,7 +4,11 @@ document.body.onkeydown = function( e ) {
         39: 'right',
         40: 'down',
         38: 'rotate',
-        32: 'drop'
+        32: 'drop',
+        65: 'left',    // Tecla A
+        68: 'right',   // Tecla D
+        83: 'down',    // Tecla S
+        87: 'rotate'   // Tecla W
     };
     if ( typeof keys[ e.keyCode ] != 'undefined' ) {
         keyPress( keys[ e.keyCode ] );
